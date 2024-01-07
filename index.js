51b47de009cd2568c5e3dc0802f08f4ce312e9f9
cynthiaspence <cynthia.spence@world50.com>
2024-01-07 12:51:22 -0500

    fixed issues with sending emails

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const nm = require('nodemailer')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const app = express()
+const port = 4000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/send-email', (req, res) => {
+  const {
+    name,
+    company,
+    email,
+    message
+  } = req.body
+
+  const transporter = nm.createTransport(
+    {
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+      }
+    }
+  )
+
+  transporter.sendMail(
+    {
+      from: 'cynthia.spence.dev@gmail.com',
+      to: process.env.GMAIL_SEND_TO,
+      subject: `${name} ${company ? `from ${company} ` : ''} is contacting you from your portfolio site.`,
+      text: `${message}\nemail: ${email}`
+    },
+    (err, info) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(info)
+      }
+    }
+  )
+
+  res.end()
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
