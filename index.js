e64820c9e9c17008815e3418f182fee926c74d99
cynthiaspence <cynthia.spence@world50.com>
2024-01-07 21:09:55 -0500

    Handle cors issue

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const nm = require('nodemailer')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 const port = 4000
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
