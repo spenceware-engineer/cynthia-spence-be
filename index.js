f618abf187a34d7800de23b3acf4a21e4dbc0e65
Cynthia Spence <cynthia.spence.dev@gmail.com>
2024-03-23 00:00:45 -0400

    Change port to 3000

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 require('dotenv').config()
 
 const app = express()
-const port = 4000
+const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
